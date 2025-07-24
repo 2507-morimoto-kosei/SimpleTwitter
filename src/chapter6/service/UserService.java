88bf50f5036a1d7277b07d0b269e33b343640dbe
morimoto_kosei <morimoto.kosei@alhinc.jp>
2025-07-24 11:46:04 +0900

    [コードレビュー依頼] 簡易Twitter実践課題1 指摘部分修正 ・isEmpty→isBlankへ修正 ・UserDaoのif文を一つにまとめる ご確認の程よろしくお願いいたします。

--- src/chapter6/service/UserService.java
+++ src/chapter6/service/UserService.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.User;
 import chapter6.dao.UserDao;
 import chapter6.logging.InitApplication;
@@ -118,7 +120,7 @@ public class UserService {
 		Connection connection = null;
 		try {
 			// パスワード暗号化、もしパスの値が空なら暗号化しない
-			if (!user.getPassword().isEmpty()) {
+			if (!StringUtils.isBlank(user.getPassword())) {
 				String encPassword = CipherUtil.encrypt(user.getPassword());
 				user.setPassword(encPassword);
 			}
