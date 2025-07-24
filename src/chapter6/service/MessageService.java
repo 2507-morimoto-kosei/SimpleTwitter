911d0ba054e0e3ca699837657f4a3f711d1e6f78
morimoto_kosei <morimoto.kosei@alhinc.jp>
2025-07-24 17:04:57 +0900

    [コードレビュー指摘修正分] Twitter実践課題2 ・MessageServiceクラスインデント修正 ご確認お願いいたします。

--- src/chapter6/service/MessageService.java
+++ src/chapter6/service/MessageService.java
@@ -67,9 +67,9 @@ public class MessageService {
 		try {
 			connection = getConnection();
 			Integer id = null;
-		    if(!StringUtils.isEmpty(userId)) {
-		        id = Integer.parseInt(userId);
-		    }
+			if(!StringUtils.isEmpty(userId)) {
+				id = Integer.parseInt(userId);
+			}
 
 			List<UserMessage> messages = new UserMessageDao().select(connection, id, LIMIT_NUM);
 			commit(connection);
